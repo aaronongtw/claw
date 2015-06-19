b9431ef5fb17047872a7e5ad956f018e59d8f0a2
Connie <conniegiann@gmail.com>
2015-06-19 16:18:04 +1000

    merge with master

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ActionController::Base
+  # protect_from_forgery with: :exception
+
+  def new
+  end
+
+  def create
+    user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user_id
+      redirect_to root_path
+    else
+      flash[:notice] = "Invalid login, please try again."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User <ActiveRecord::Base
    has_and_belongs_to_many :games
    has_many :highscores
    has_many :vouchers
+
+   has_secure_password
+   validates :email, :presence => true
+   validates :email, :uniqueness => true
 end
