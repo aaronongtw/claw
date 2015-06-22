c02521ba1549e903f7d496f867bb58061c60590f
Rodney S <write2rs2@outlook.com>
2015-06-22 11:19:55 +1000

    geocoder

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,8 @@
 class Client <ActiveRecord::Base
    has_many :vouchers
    has_and_belongs_to_many :games
+
+   geocoded_by :location
+   after_validation :geocode,
+   :if => lambda{ |obj| obj.address_changed? }
 end
