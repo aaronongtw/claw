c4385311c598428dfed970a9c7dfe80525e58c00
AaronOngTW <aaronongtw@gmail.com>
2015-06-23 13:37:07 +1000

    redemption seperation logic added

--- app/assets/javascripts/redeem.js
+++ app/assets/javascripts/redeem.js
@@ -11,11 +11,17 @@ var redeemList = function() {
             url: '/uservouchers',
         }).done(function(data) { 
             $('#gameBox').append('<div id="redeem-list"></div>')
-            data = data.filter(function (el) {
+            RedeemData = data.filter(function (el) {
               return el.client_id != 4
             });
-              for (var i = 0 ; i < data.length; i += 1){
-                 $('#redeem').append('<div id='+ data[i].id +'>' + data[i].name + ' ' + data[i].description + '</div>')
+              for (var i = 0 ; i < RedeemData.length; i += 1){
+                 $('#redeem').append('<div id='+ RedeemData[i].id +'>' + RedeemData[i].name + ' ' + RedeemData[i].description + '</div>')
+              }
+            DigitalData = data.filter(function (el) {
+              return el.client_id == 4
+            })
+              for (var i = 0 ; i < DigitalData.length; i += 1){
+                 $('#redeem').append('<img src='+ DigitalData[i].textstuff +'>' + '<div id='+ DigitalData[i].id +'>' + DigitalData[i].name + ' ' + DigitalData[i].description + '</div>' )
               }
             })
         }
